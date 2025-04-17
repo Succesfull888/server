b340557ae21c81f08aeb4d9cb83f9c741ed77bfc
Marufbek Irgashev <68223774+Succesfull888@users.noreply.github.com>
2025-04-17 17:02:19 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://new-cefr-exam.vercel.app',
+  credentials: true
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
